491fe2b8ca446213acc65f4e12f02b4a8f394f92
Alex May <alex.k.may@gmail.com>
2015-11-27 22:07:03 -0800

    setup spacemacs

--- setup.sh
+++ setup.sh
@@ -15,3 +15,9 @@ cd `dirname $0`
   echo Installing atom && \
   git clone git@github.com:substantial/atomfiles.git ~/.atom && \
   cd ~/.atom && bin/update-packages )
+
+[[ -d ~/.emacs.d ]] || ( \
+  echo Installing spacemacs && \
+  git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d && \
+  cd ~/.emacs.d && \
+  git checkout --track -B develop origin/develop)

3e570105c56c1d266a36adf56d755fee1851d547
Alex May <alex.k.may@gmail.com>
2015-11-27 21:12:07 -0800

    add info echo for vim/atom install steps

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@ cd `dirname $0`
 ./setup/mac-defaults.sh
 
 [[ -d ~/.vim ]] || ( \
+  echo Installing vim && \
   git clone git@github.com:amay/vimfiles.git ~/.vim && \
   ~/.vim/setup.sh )
 
 [[ -d ~/.atom ]] || ( \
+  echo Installing atom && \
   git clone git@github.com:substantial/atomfiles.git ~/.atom && \
   cd ~/.atom && bin/update-packages )
